b791f4bd972f2f1f648f4426de9d2577fe8418d5
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-07-21 18:59:26 +0000

    Edit File App

--- Learn-React/src/App.jsx
+++ Learn-React/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import PostDetails from "./pages/PostDetails";
 
-export default function App() {
-  
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <div className="container" style={{ padding: "20px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
 
